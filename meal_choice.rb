b4e45bffaeee36b68b67381bee511452b6f683cd
Sarah Li <sarcl@hotmail.com>
2018-06-22 13:15:33 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -40,3 +40,4 @@ dinner
 ## This returns "Any time, is the right time for Sweedish fish!"
 # Call your methods without any arguments here
 # Define breakfast, lunch and dinner methods that return the meal choice passed into them. If nothing is passed in, it shoud default to the foods on the readme (frosted flake, grilled cheese, salmon)
+# Here's an example of a 'snacks' method that returns the meal choice passed in to it and defaults to "cheetos" if nothing is passed in.
